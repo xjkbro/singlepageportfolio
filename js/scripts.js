2c3e2d02090f512e3e79d2c99511de74442a68e2
Jason-Kyle De Lara <delara@unlv.nevada.edu>
2020-11-07 11:25:44 -0800

    2nd commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,9 @@
-$(function() {
-    $(".typed").typed({ strings: ["My Name is Gabriel", "I'm a Developer", "Let's build something awesome"] });
-    
+$(function () {
+    $(".typed").typed({
+        strings: ["My Name is Jason-Kyle De Lara", "I'm a developer"],
+        typeSpeed: 75,
+        backDelay: 500,
+        // loop
+        loop: true
+    });
 });
